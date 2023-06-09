910a137922c7694351c468b9f2c2962c7e2d7270
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-09 22:06:13 +0700

    4. update data source config
    use function to support async factory for nestjs.
    this is caused process.env in nest js is not available on initial state.
    hence changing it to function and use useFactory and install
    nestjs/config will make process.env available to be use.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloWorldController } from './hello-world/hello-world.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { datasourceConfig } from './config/typeorm-datasource.js';
 import { CustomerModule } from './customer/customer.module';
 import { OrderModule } from './order/order.module';
 import { SupplierModule } from './supplier/supplier.module';
 import { ProductModule } from './product/product.module';
 import { LocationModule } from './location/location.module';
+import { ConfigModule } from '@nestjs/config';
+import { datasourceConfig } from './config/typeorm-datasource';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(datasourceConfig), CustomerModule, OrderModule, SupplierModule, ProductModule, LocationModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => datasourceConfig(),
+    }),
+    CustomerModule,
+    OrderModule,
+    SupplierModule,
+    ProductModule,
+    LocationModule,
+  ],
   controllers: [AppController, HelloWorldController],
   providers: [AppService],
 })
--- src/config/typeorm-datasource.ts
+++ src/config/typeorm-datasource.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export const datasourceConfig: PostgresConnectionOptions = {
+export const datasourceConfig = (): PostgresConnectionOptions => ({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['../**/*.entity.ts'],
-};
+  entities: ['dist/**/*.entity.js'],
+  logging: 'all',
+});
 
-const datasource = new DataSource(datasourceConfig);
+const datasource = new DataSource(datasourceConfig());
 
 export default datasource;
