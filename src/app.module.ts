c5e2ded71719656c5a9d1d1d53bb1f8bdff309d8
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-09 22:08:50 +0700

    remove bloat files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HelloWorldController } from './hello-world/hello-world.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerModule } from './customer/customer.module';
 import { OrderModule } from './order/order.module';
@@ -23,7 +20,7 @@ import { datasourceConfig } from './config/typeorm-datasource';
     ProductModule,
     LocationModule,
   ],
-  controllers: [AppController, HelloWorldController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
