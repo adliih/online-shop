fd880d5175ee5e36517645dd53e52bb8b96af056
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-09 17:36:25 +0700

    4. add typeorm scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloWorldController } from './hello-world/hello-world.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { datasourceConfig } from './config/typeorm-datasource.js';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [],
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(datasourceConfig)],
   controllers: [AppController, HelloWorldController],
   providers: [AppService],
 })
---
+++ src/config/typeorm-datasource.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export const datasourceConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
+};
+
+const datasource = new DataSource(datasourceConfig);
+
+export default datasource;
