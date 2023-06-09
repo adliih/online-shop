b8cad0f88385b1ac934e51c5f71c24428d985d89
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-06-09 17:12:03 +0700

    4. add db
    - using typeorm library with postgres configuration
    - also include docker-compose to support local development

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloWorldController } from './hello-world/hello-world.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+    }),
+  ],
   controllers: [AppController, HelloWorldController],
   providers: [AppService],
 })
